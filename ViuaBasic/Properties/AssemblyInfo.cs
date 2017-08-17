e2d8e79226feca678963ce79d9a71750078c19fe
vktgz <vktgz@rocday.pl>
2017-08-17 15:04:15 +0200

    prepare dev release

--- ViuaBasic/Properties/AssemblyInfo.cs
+++ ViuaBasic/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.CompilerServices;
 // Change them to the values specific to your project.
 
 [assembly: AssemblyTitle ("ViuaBasic")]
-[assembly: AssemblyDescription ("")]
+[assembly: AssemblyDescription ("Retro style BASIC dialect for ViuaVM")]
 [assembly: AssemblyConfiguration ("")]
 [assembly: AssemblyCompany ("")]
 [assembly: AssemblyProduct ("")]
@@ -17,7 +17,7 @@ using System.Runtime.CompilerServices;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion ("0.1.0.0")]
+[assembly: AssemblyVersion ("0.0.1.0")]
 
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
